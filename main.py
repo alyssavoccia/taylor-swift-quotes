7bcfc89b6359b028f0ecb862a750de48ecdcce53
Alyssa Voccia <alyssavoccia@Alyssas-MacBook-Pro-2.local>
2025-01-23 21:09:15 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+def get_quote():
+    pass
+    #Write your code here.
+
+
+
+window = Tk()
+window.title("Taylor Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Taylor Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+taylor_img = PhotoImage(file="taylor.png")
+taylor_img = taylor_img.subsample(4, 4)
+taylor_button = Button(image=taylor_img, highlightthickness=0, command=get_quote)
+taylor_button.grid(row=1, column=0)
+
+
+
+window.mainloop()
